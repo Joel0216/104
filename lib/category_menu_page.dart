6c8ba4bfb0a1a6c8347259f8e699829fb30ecaa9
Joel <joelantoniopool@gmail.com>
2025-10-02 00:03:31 -0600

    Se completó la funcionalidad X y se corrigió el bug Y

--- lib/category_menu_page.dart
+++ lib/category_menu_page.dart
@@ -1,11 +1,26 @@
-import 'package:flutter/material.dart';
+// Copyright 2018-present the Flutter authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
+import 'package:flutter/material.dart';
 import 'colors.dart';
 import 'model/product.dart';
 
 class CategoryMenuPage extends StatelessWidget {
   final Category currentCategory;
   final ValueChanged<Category> onCategoryTap;
+  // La lista de categorías incluye todas las opciones que pediste:
+  // all, accessories, clothing, home.
   final List<Category> _categories = Category.values;
 
   const CategoryMenuPage({
@@ -15,38 +30,40 @@ class CategoryMenuPage extends StatelessWidget {
   }) : super(key: key);
 
   Widget _buildCategory(Category category, BuildContext context) {
+    // Convierte el enum a un String legible: Category.all -> ALL
     final categoryString =
         category.toString().replaceAll('Category.', '').toUpperCase();
     final ThemeData theme = Theme.of(context);
+    final isSelected = category == currentCategory;
 
     return GestureDetector(
+      // Al tocar, llama a la función de callback para cambiar la categoría en Home.dart
       onTap: () => onCategoryTap(category),
-      child: category == currentCategory
-          ? Column(
-              children: <Widget>[
-                const SizedBox(height: 16.0),
-                Text(
-                  categoryString,
-                  style: theme.textTheme.headlineMedium,
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(height: 14.0),
-                Container(
-                  width: 70.0,
-                  height: 2.0,
-                  color: kShrinePink400,
-                ),
-              ],
-            )
-          : Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: Text(
-                categoryString,
-                style: theme.textTheme.headlineMedium!.copyWith(
-                    color: kShrineBrown900.withAlpha(153)),
-                textAlign: TextAlign.center,
-              ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Column(
+          children: <Widget>[
+            const SizedBox(height: 16.0),
+            Text(
+              categoryString,
+              style: theme.textTheme.headlineMedium!.copyWith(
+                  // Hace que el texto seleccionado sea más oscuro
+                  color: isSelected
+                      ? kShrineBrown900
+                      : kShrineBrown900.withAlpha(153)),
+              textAlign: TextAlign.center,
             ),
+            const SizedBox(height: 14.0),
+            // Muestra la línea rosa debajo de la categoría seleccionada
+            if (isSelected)
+              Container(
+                width: 70.0,
+                height: 2.0,
+                color: kShrinePink400,
+              ),
+          ],
+        ),
+      ),
     );
   }
 
@@ -55,8 +72,9 @@ class CategoryMenuPage extends StatelessWidget {
     return Center(
       child: Container(
         padding: const EdgeInsets.only(top: 40.0),
-        color: kShrinePink100, // Color de fondo rosa claro
+        color: kShrinePink100, // Color de fondo del menú
         child: ListView(
+            // Mapea la lista de categorías a widgets
             children: _categories
                 .map((Category c) => _buildCategory(c, context))
                 .toList()),
--- lib/model/product.dart
+++ lib/model/product.dart
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-// http://www.apache.org/licenses/LICENSE-2.0
+// http://www.apache.org/licenses/LICENSE-20.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 enum Category {
+  // Las categorías ya estaban definidas para todos, accesorios, ropa y hogar.
   all,
   accessories,
   clothing,
